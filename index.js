15259657891c3e5fbd0961ac317041682e6cc56b
Adelina <70908927+Adelina122@users.noreply.github.com>
2023-08-02 12:12:12 +0200

    add random advice logic

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const API_URL = "";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    
+    // res.send("Hello");
+    res.render("index.ejs", { content: "Click a button" });
+});
+
+app.post("/get-advice", async (req, res) => {
+    try {
+        const result = await axios.get("https://api.adviceslip.com/advice");
+        res.render("index.ejs", { content: JSON.stringify(result.data.slip.advice) });
+    } catch (error) {
+        res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+    }
 });
 
 app.listen(port, () => {
